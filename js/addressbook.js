906853c3c9df98230e3bbe432f5a15b25ff79af0
Geetha Pai <geetha.pai@gmail.com>
2014-03-05 21:45:54 -0500

    added ajax & split json

--- js/addressbook.js
+++ js/addressbook.js
@@ -1,53 +1,63 @@
-/*start of anonymous function to contain all variables*/
+/*standard Ajax xhr function*/
+
+function getHTTPObject() {
+
+		var xhr;
+
+		if(window.XMLHttpRequest) {//check for support
+
+			//if it's supported, use it because it's better
+			xhr = new XMLHttpRequest();
+
+	} else if (window.ActiveXObject) { //check for IE 6 Ajax
+
+			//save it to the xhr variable
+			xhr = new ActiveXObject("Msxml2.XMLHTTP");
+	}
+		//spit out the correct one so we can use it
+		return xhr;
+}
+
+/*define the Ajax call*/
+
+function ajaxCall(dataUrl, outputElement, callback) {
+
+	/*	use our function to get the correct Ajax object based on support*/
+	var request = getHTTPObject();
+
+	outputElement.innerHTML = "Loading...";
+
+	request.onreadystatechange = function() {
+
+		//check to see if the Ajax call went through
+		if (request.readyState === 4 && request.status === 200) {
+
+			//save the ajax response to a variable
+			var contacts = JSON.parse(request.responseText);
+
+			//make sure the callback is indeed a function before executing it
+			if(typeof callback === "function") {
+
+						callback(contacts);
+			} //end checking function
+
+		} // end ajax status check
+
+	} // end onreadystatechange
+
+	request.open("GET", dataUrl, true);
+	request.send(null);
+} // end function
+
+
+/*start of anonymous function to contain variables*/
 (function() {
 
-/*JSON data object with contacts*/
-var contacts = {
-	"addressBook" : [
-			{
-				"name": "anand",
-				"email": "anand@example.com"
-			},
-			{
-				"name": "brenda",
-				"email": "brenda@example.com"
-			},
-			{
-				"name": "bart",
-				"email": "bsimpson@example.com"
-			},
-			{
-				"name": "jack",
-				"email": "jspencer@example.com"
-			},
-			{
-				"name": "jennifer",
-				"email": "jam@example.com"
-			},
-			{
-				"name": "marisa",
-				"email": "marisa@example.com"
-			},
-			{
-				"name": "michael",
-				"email": "patterson@example.com"
-			},
-			{
-				"name": "sabina",
-				"email": "sbrown@example.com"
-			},
-			{
-				"name": "travis",
-				"email": "travis@example.com"
-			}
-	]
-};
 
  // save DOM elements and common variables
 var searchForm = document.getElementById("search-form"),
 		searchField = document.getElementById("q"),
 		getAllButton = document.getElementById("get-all"),
-		count = contacts.addressBook.length,
 		target = document.getElementById("output");
 
 
@@ -58,81 +68,104 @@ var addr = {
 	//define the method for submit
 	search : function(event){
 
-		//save the input value, contacts length and i to variables
-		var searchValue = searchField.value,
-			i;
+		/*set the output element*/
+		var output = document.getElementById("output");		
 
-		//stop the default behavior
-		event.preventDefault();
+		/*start the Ajax call*/
+		ajaxCall('data/contacts.json', output, function(data) {
 
-		//clear the target area incase there's something in it
-		target.innerHTML = "";
+			//save the input value, contacts length and i to variables
+			var searchValue = searchField.value,
+					addrBook = data.addressBook,
+					count = addrBook.length,
+					i;
 
-		//check the count
-		if(count > 0 && searchValue !== ""){
+			//stop the default behavior
+			event.preventDefault();
 
-			//loop through the contacts
-			for(i = 0; i < count; i = i + 1) { 	
+			//clear the target area incase there's something in it
+			target.innerHTML = "";
+
+			//check the count
+			if(count > 0 && searchValue !== ""){
+
+				//loop through the contacts
+				for(i = 0; i < count; i = i + 1) { 	
+				
+					//look through the name value to see if contains the search term string
+					var obj = addrBook[i],
+						isItFound = obj.name.indexOf(searchValue);
+
+					//anything other than -1 means there is a match
+					if(isItFound !== -1) {
+						target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>';
+					} //end if
+				
+				} // end for loop
 			
-				//look through the name value to see if contains the search term string
-				var obj = contacts.addressBook[i],
-					isItFound = obj.name.indexOf(searchValue);
+			} // end count check
 
-				//anything other than -1 means there is a match
-				if(isItFound !== -1) {
-					target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a></p>';
-				} //end if
-			
-			} // end for loop
-		
-		} // end count check
+		}); // end ajax call	
 
 	},
 
+
 		getAllContacts : function() {
 
-			/*ready the loop*/
-			var i;
+			/*set the output element*/
+			var output = document.getElementById("output");
 
-			/*clear the contents of #output just in case there's something in there*/
-			target.innerHTML = "";
-			
-			/*checking count before looping*/
-			if(count > 0){
-				/*loop through the contacts*/
-				for(i = 0; i < count; i = i + 1) {
-					
-					var obj = contacts.addressBook[i];
+			/*start the Ajax call*/
+			ajaxCall('data/contacts.json', output, function(data) {
 
-					target.innerHTML +=	'<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a></p>';
-
-				} // end for loop
-			} // end of count check
-		},
+					var addrBook = data.addressBook,
+							count = addrBook.length,
+							i;
 		
 
-setActiveSection: function() {
-	// add a class of "active" the wrapping div
-	this.parentNode.setAttribute("class", "active");
-},
+					/*clear the contents of #output just in case there's something in there*/
+					target.innerHTML = "";
+					
+					/*checking count before looping*/
+					if(count > 0){
 
-removeActiveSection: function(){
+							/*loop through the contacts*/
+							for(i = 0; i < count; i = i + 1) {
+								
+								/*look through the name value to see if it contains the search term string*/
+								var obj = addrBook[i];
 
-	// remove the class from the wrapping div
-	this.parentNode.removeAttribute("class");
-},
+								target.innerHTML +=	'<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>';
 
-addHoverClass: function() {
-	// add a class of "hovering" to the wrapping div
-	searchForm.setAttribute("class", "hovering");
-},
+						} // end for loop
+					} // end of count check
+				
+				}); // end ajax call
+			},
+		
 
-removeHoverClass: function() {
-	// remove all classes from the wrapping div
-	searchForm.removeAttribute("class");
-}
+		setActiveSection: function() {
+			// add a class of "active" the wrapping div
+			this.parentNode.setAttribute("class", "active");
+		},
 
-}; // end adr object
+		removeActiveSection: function(){
+
+			// remove the class from the wrapping div
+			this.parentNode.removeAttribute("class");
+		},
+
+		addHoverClass: function() {
+			// add a class of "hovering" to the wrapping div
+			searchForm.setAttribute("class", "hovering");
+		},
+
+		removeHoverClass: function() {
+			// remove all classes from the wrapping div
+			searchForm.removeAttribute("class");
+		}
+
+}; // end addr object
 
 /*BELOW are the event listeners*/
 /*activate autocomplete on keyUp*/
