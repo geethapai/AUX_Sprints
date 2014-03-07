a5a63a145df3469106e5ef666a2c1bc3b4cdcaf9
Geetha Pai <geetha.pai@gmail.com>
2014-03-07 12:13:33 -0500

    added jquery

--- js/addressbook.js
+++ js/addressbook.js
@@ -1,107 +1,44 @@
-/*standard Ajax xhr function*/
+/*Using JQuery*/
 
-function getHTTPObject() {
-
-		var xhr;
-
-		if(window.XMLHttpRequest) {//check for support
-
-			//if it's supported, use it because it's better
-			xhr = new XMLHttpRequest();
-
-	} else if (window.ActiveXObject) { //check for IE 6 Ajax
-
-			//save it to the xhr variable
-			xhr = new ActiveXObject("Msxml2.XMLHTTP");
-	}
-		//spit out the correct one so we can use it
-		return xhr;
-}
-
-/*define the Ajax call*/
-
-function ajaxCall(dataUrl, outputElement, callback) {
-
-	/*	use our function to get the correct Ajax object based on support*/
-	var request = getHTTPObject();
-
-	outputElement.innerHTML = "Loading...";
-
-	request.onreadystatechange = function() {
-
-		//check to see if the Ajax call went through
-		if (request.readyState === 4 && request.status === 200) {
-
-			//save the ajax response to a variable
-			var contacts = JSON.parse(request.responseText);
-
-			//make sure the callback is indeed a function before executing it
-			if(typeof callback === "function") {
-
-						callback(contacts);
-			} //end checking function
-
-		} // end ajax status check
-
-	} // end onreadystatechange
-
-	request.open("GET", dataUrl, true);
-	request.send(null);
-} // end function
-
-
-/*start of anonymous function to contain variables*/
-(function() {
-
-
- // save DOM elements and common variables
-var searchForm = document.getElementById("search-form"),
-		searchField = document.getElementById("q"),
-		getAllButton = document.getElementById("get-all"),
-		target = document.getElementById("output");
-
-
-/*define the adr object to hold methods*/
+(function(){
+/*define the addressbook object to hold methods*/
 
 var addr = {
 
 	//define the method for submit
 	search : function(event){
 
-		/*set the output element*/
-		var output = document.getElementById("output");		
-
-		/*start the Ajax call*/
-		ajaxCall('data/contacts.json', output, function(data) {
-
-			//save the input value, contacts length and i to variables
-			var searchValue = searchField.value,
-					addrBook = data.addressBook,
-					count = addrBook.length,
-					i;
-
-			//stop the default behavior
+		//stop the default behavior
 			event.preventDefault();
 
+		/*start the Ajax call*/
+		$.getJSON('data/contacts.json', function(data) {
+
+			//save the input value, contacts length and i to variables
+			var searchValue = $('#q').val(),
+					addrBook = data.addressBook,
+					count = addrBook.length;
+
+
 			//clear the target area incase there's something in it
-			target.innerHTML = "";
+			$('#output').empty();
 
 			//check the count
 			if(count > 0 && searchValue !== ""){
 
 				//loop through the contacts
-				for(i = 0; i < count; i = i + 1) { 	
+				$.each(addrBook, function (i , obj) { 	
 				
 					//look through the name value to see if contains the search term string
-					var obj = addrBook[i],
-						isItFound = obj.name.indexOf(searchValue);
+					var isItFound = obj.name.indexOf(searchValue);
 
 					//anything other than -1 means there is a match
 					if(isItFound !== -1) {
-						target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>';
+						$('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>');
+			
 					} //end if
 				
-				} // end for loop
+				}); // end each 
 			
 			} // end count check
 
@@ -110,83 +47,57 @@ var addr = {
 	},
 
 
-		getAllContacts : function() {
+		getAllContacts: function() {
 
-			/*set the output element*/
-			var output = document.getElementById("output");
-
-			/*start the Ajax call*/
-			ajaxCall('data/contacts.json', output, function(data) {
+			/*start Ajax call*/
+			$.getJSON('data/contacts.json', function (data) {
 
 					var addrBook = data.addressBook,
-							count = addrBook.length,
-							i;
-		
+							count = addrBook.length;
+							
 
 					/*clear the contents of #output just in case there's something in there*/
-					target.innerHTML = "";
+					$('#output').empty();
 					
 					/*checking count before looping*/
 					if(count > 0){
 
 							/*loop through the contacts*/
-							for(i = 0; i < count; i = i + 1) {
-								
-								/*look through the name value to see if it contains the search term string*/
-								var obj = addrBook[i];
+							$.each(addrBook, function (i, obj) {
 
-								target.innerHTML +=	'<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>';
+								 $('#output').append('<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>');
 
-						} // end for loop
+						}); // end each
+
 					} // end of count check
 				
+
 				}); // end ajax call
-			},
+			
+			}
 		
-
-		setActiveSection: function() {
-			// add a class of "active" the wrapping div
-			this.parentNode.setAttribute("class", "active");
-		},
-
-		removeActiveSection: function(){
-
-			// remove the class from the wrapping div
-			this.parentNode.removeAttribute("class");
-		},
-
-		addHoverClass: function() {
-			// add a class of "hovering" to the wrapping div
-			searchForm.setAttribute("class", "hovering");
-		},
-
-		removeHoverClass: function() {
-			// remove all classes from the wrapping div
-			searchForm.removeAttribute("class");
-		}
-
-}; // end addr object
-
-/*BELOW are the event listeners*/
-/*activate autocomplete on keyUp*/
-searchField.addEventListener("keyup", addr.search, false);
-
-/*activate the focus event on the search box*/
-searchField.addEventListener("focus", addr.setActiveSection, false);
-
-/*remove the blur event on the search box*/
-searchField.addEventListener("blur", addr.removeActiveSection, false);
-
-getAllButton.addEventListener("click", addr.getAllContacts, false);
-
-//activate the focus event on the search box
-searchForm.addEventListener("mouseover", addr.addHoverClass, false);
-
-//activate the blur event on the search box
-searchForm.addEventListener("mouseout", addr.removeHoverClass, false);
-
-searchForm.addEventListener("submit", addr.search, false);
+		}; // end addr object
 
 
+		//searchbox events
+		$('#q').keyup(addr.search).focus(function() {
 
-})(); // end anonymous function
+			$(this).parent().addClass("active");
+		
+		}).blur(function(){
+
+			$(this).parent().removeClass("active");
+		});
+
+		//search form events
+		$("#search-form").hover(function() {
+
+			$(this).addClass("hovering");
+
+		}).submit(addr.search);
+
+
+		//get all contacts action
+		$("#get-all").click(addr.getAllContacts);
+
+	})(); // end anonymous function
